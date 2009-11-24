4e7b78db4ac03fb48a3432136e7d35b649583303
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-11-24 11:18:07 +0000

    Initial kickstarter code
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tagpack_altselector/trunk@26893 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,45 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "tagpack_altselector"
+#
+# Auto generated 24-11-2009 12:15
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Alternate tag selector for Tag Pack',
+	'description' => 'Displays the fulll list of tags with checkboxes for selection, instead of the traditional selector.',
+	'category' => 'be',
+	'author' => 'Francois Suter (Cobweb)',
+	'author_email' => 'typo3@cobweb.ch',
+	'shy' => '',
+	'dependencies' => 'tagpack',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'tagpack' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"1ae0";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"ce60";s:20:"doc/wizard_form.html";s:4:"8208";}',
+);
+
+?>
