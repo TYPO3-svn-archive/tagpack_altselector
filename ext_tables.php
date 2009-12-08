b1b0a61a33624ecdeca76054f5ea5999e4a3fbe4
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-12-08 14:54:25 +0000

    Changed alternate TCA configuration definition to user T3_VAR
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tagpack_altselector/trunk@27464 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -3,15 +3,15 @@
 require_once(t3lib_extMgm::extPath('tagpack_altselector', 'class.tx_tagpackaltselector_tceforms.php'));
 
 	// Define alternate TCA configuration for tag selector
-t3lib_div::loadTCA('tx_tagpack_tags');
-$TCA['tx_tagpack_tags']['tagpack_options']['altselector'] = $TCA['tx_tagpack_tags']['columns']['relations'];
-$TCA['tx_tagpack_tags']['tagpack_options']['altselector']['exclude'] = 0;
-$TCA['tx_tagpack_tags']['tagpack_options']['altselector']['config']['type'] = 'user';
-$TCA['tx_tagpack_tags']['tagpack_options']['altselector']['config']['userFunc'] = 'tx_tagpackaltselector_TCEForms->selectorField';
-$TCA['tx_tagpack_tags']['tagpack_options']['altselector']['config']['allowed'] = 'tx_tagpack_tags';
-$TCA['tx_tagpack_tags']['tagpack_options']['altselector']['config']['prepend_tname'] = 0;
-$TCA['tx_tagpack_tags']['tagpack_options']['altselector']['label'] = $TCA['tx_tagpack_tags']['ctrl']['title'];
+$alternateConfiguration = array(
+								'label' => $TCA['tx_tagpack_tags']['ctrl']['title'],
+								'exclude' => 0,
+								'config' => array(
+									'type' => 'user',
+									'userFunc' => 'tx_tagpackaltselector_TCEForms->selectorField',
+								)
+							);
 
 	// Set the alternate configuration for the pages table
-$TCA['tx_tagpack_tags']['tagpack_options']['alternateTCA']['pages'] = $TCA['tx_tagpack_tags']['tagpack_options']['altselector'];
+$T3_VAR['EXT']['tagpack']['TCA']['alternate_config']['pages'] = $alternateConfiguration;
 ?>
